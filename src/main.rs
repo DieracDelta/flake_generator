b8965887bf9f9f732ef2f919a51f3be8e3edcf58
Justin Restivo <justin.p.restivo@gmail.com>
2021-04-11 15:59:26 -0400

    working AST search

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
 #![allow(dead_code)]
 #[warn(unused_variables)]
 extern crate skim;
+extern crate rowan;
 use skim::prelude::*;
 use std::io::Cursor;
 use std::{env, error::Error, fs};
 
 use smol_str::SmolStr;
-use rnix::{types::*, NodeOrToken, SyntaxKind::*, SyntaxNode};
+use rnix::{types::*, NodeOrToken, SyntaxKind::*, SyntaxNode, NixLanguage};
 
 enum UserAction {
     AddDep,
@@ -28,18 +29,18 @@ fn get_prompts(action: UserAction) -> Vec<Vec<String>> {
     match action {
         UserAction::AddDep =>
             vec![vec!["Add a dependency to your flake.".to_string(),
-                      "Please select an package from nixpkgs.".to_string()]],
-        UserAction::RemoveDep =>
-            vec![vec!["Remove a dependency from your flake. ".to_string(),
-                      "Please select a input to remove.".to_string()]],
-        UserAction::AddInput =>
-            vec![vec!["Add an input to your flake.".to_string(),
-                      "Please input a flake url and indicate if it's a flake".to_string()],
-                 vec!["Is the input a flake?".to_string()]],
-        UserAction::RemoveInput =>
-            vec![vec!["Remove an input from your flake.".to_string(), "Please select an input to remove.".to_string()]],
-        UserAction::GenLib => unimplemented!(),
-        UserAction::GenBin(_) => unimplemented!()
+            "Please select an package from nixpkgs.".to_string()]],
+            UserAction::RemoveDep =>
+                vec![vec!["Remove a dependency from your flake. ".to_string(),
+                "Please select a input to remove.".to_string()]],
+                UserAction::AddInput =>
+                    vec![vec!["Add an input to your flake.".to_string(),
+                    "Please input a flake url and indicate if it's a flake".to_string()],
+                    vec!["Is the input a flake?".to_string()]],
+                    UserAction::RemoveInput =>
+                        vec![vec!["Remove an input from your flake.".to_string(), "Please select an input to remove.".to_string()]],
+                        UserAction::GenLib => unimplemented!(),
+                        UserAction::GenBin(_) => unimplemented!()
     }
 }
 
@@ -66,16 +67,84 @@ fn query_user_input(prompt: Vec<String>, items: Vec<String>) -> String {
         .unwrap().query
 }
 
-fn search_for_attr(attr: String, depth: usize, root_node: &AttrSet) -> Result<(&AttrSet, String), &str> {
-    let stack: Vec<u64> = root_node.entries().into_vec();
-    let remaining_items: usize;
-    //for entry_wrapped in set.entries() {
-        //let entry = entry_wrapped.value().unwrap();
-
-    //}
-
-    Err("unimplemented")
+fn get_str_val(node: &rowan::api::SyntaxNode<NixLanguage>) -> Result<String, String>{
+    // TODO the clone is probably wrong
+    Str::cast((*node).clone()).unwrap().parts().fold();
+    println!("attr value is {:?}", Str::cast(cur_node_value).unwrap().parts());
 }
+
+/// TODO docstring
+fn search_for_attr(attr: String, max_depth: usize,
+    root_node: &rowan::api::SyntaxNode<NixLanguage>, exact_depth: Option<usize>)
+    -> Result<Vec<rowan::api::SyntaxNode<NixLanguage>>, String> {
+        // try to get out a attrset.
+        // TODO the clone is probably wrong
+        let mut stack = match AttrSet::cast((*root_node).clone()) {
+            Some(rn) => rn.entries().collect(),
+            None => Vec::new()
+        };
+        // it's a tree so this works
+        let mut remaining_items_prev: usize = stack.len();
+        let mut remaining_items_cur: usize = 0;
+        let mut cur_depth = 0;
+        let mut result = Vec::new();
+        while !stack.is_empty() {
+            // TODO note that we're popping off the back
+            let cur_node = stack.pop().unwrap();
+            let cur_node_value = cur_node.value().unwrap();
+            let cur_node_key = cur_node.key().unwrap();
+            if remaining_items_prev == 0 {
+                remaining_items_prev = remaining_items_cur - 1;
+                remaining_items_cur = 0;
+                cur_depth += 1;
+            }
+            if cur_depth > max_depth {
+                //return Err(format!("Attribute {} does not exist at depth {:?}", attr, max_depth));
+                return Ok(vec![]);
+            }
+
+            let mut real_depth = cur_depth -1;
+            let mut cur_node_attribute = "".to_string();
+            // TODO url.url.url will break this
+            for p in cur_node_key.path(){
+                let tmp = Ident::cast(p).unwrap();
+                let cur_attr = tmp.as_str();
+                cur_node_attribute.push_str(".");
+                cur_node_attribute.push_str(&cur_attr);
+                real_depth += 1;
+                if attr == cur_attr {
+                    break;
+                }
+            }
+
+            let is_match = cur_node_attribute == attr && exact_depth.map_or(true, |x| -> bool {x == real_depth});
+            if is_match {
+                result.push(cur_node_value);
+            } else {
+                match cur_node_value.kind() {
+                    NODE_ATTR_SET => {
+                        let cur_node_casted = AttrSet::cast(cur_node_value.clone()).unwrap();
+                        if is_match {
+                            result.push(cur_node_value);
+                        } else {
+                            cur_node_casted.entries()
+                                .for_each(|entry| {
+                                    stack.insert(0, entry);
+                                    remaining_items_cur += 1;
+                                });
+                        }
+                    }
+                    NODE_STRING => {
+                        // do nothing...
+                        //println!("attr value is {:?}", Str::cast(cur_node_value).unwrap().parts());
+                    }
+                    _kind => () // println!("other kind: {:?}", _kind)
+                }
+            }
+        }
+        Ok(result)
+
+    }
 
 
 fn main() -> Result<(), Box<dyn Error>> {
@@ -85,36 +154,42 @@ fn main() -> Result<(), Box<dyn Error>> {
         Some(file) => file,
         None => {
             eprintln!("Usage: list-fns <file>");
-            Ok(())
+            return Ok(());
         }
     };
     let content = fs::read_to_string(&file)?;
     let ast = rnix::parse(&content).as_result()?;
     // how to get it back
     //println!("{}", ast.node());
-    let set = ast.root().inner().and_then(AttrSet::cast).ok_or("root isn't a set")?;
-    println!("set is: {:?}", set);
+    let set = ast.root().inner().unwrap();
+    let input_attrs = search_for_attr("inputs".to_string(), 1, &set, Some(0)).unwrap();
+    let url_attrs = input_attrs.iter().fold(Vec::new(),
+    | mut acc, ele | -> Vec<_> {
+        acc.append(&mut search_for_attr("url".to_string(), 10, ele, None).unwrap()); acc
+    });
+    println!("urls found: {:?}", url_attrs);
 
-    for entry_wrapped in set.entries() {
-        let entry = entry_wrapped.value().unwrap();
-        println!("entry key is: {:?}",  entry_wrapped.key().unwrap().path().next().and_then(Ident::cast).unwrap().as_str());
 
-        match entry.kind() {
-            NODE_ATTR_SET => {
-                //println!("top attr is {:?}",  AttrSet::cast(entry).unwrap().key());
-                for input_kvs in AttrSet::cast(entry).unwrap().entries(){
-                    // TODO do fold and separate into method
-                    input_kvs.key().unwrap().path().for_each(|ele| {println!("key value is {:?}", Ident::cast(ele).unwrap().as_str())});
-                    // morally speaking, should do this recursively, probably
-                    println!("attr value is {:?}", input_kvs.value().and_then(Str::cast).unwrap().parts());
-                };
-            },
-            _ => {
-                println!("kind is {:?}", entry.kind());
-                println!("entry is {:?}", entry.text());
-            }
-        };
-    }
+    //for entry_wrapped in set.entries() {
+    //let entry = entry_wrapped.value().unwrap();
+    //println!("entry key is: {:?}",  entry_wrapped.key().unwrap().path().next().and_then(Ident::cast).unwrap().as_str());
+
+    //match entry.kind() {
+    //NODE_ATTR_SET => {
+    ////println!("top attr is {:?}",  AttrSet::cast(entry).unwrap().key());
+    //for input_kvs in AttrSet::cast(entry).unwrap().entries(){
+    //// TODO do fold and separate into method
+    //input_kvs.key().unwrap().path().for_each(|ele| {println!("key value is {:?}", Ident::cast(ele).unwrap().as_str())});
+    //// morally speaking, should do this recursively, probably
+    //println!("attr value is {:?}", input_kvs.value().and_then(Str::cast).unwrap().parts());
+    //};
+    //},
+    //_ => {
+    //println!("kind is {:?}", entry.kind());
+    //println!("entry is {:?}", entry.text());
+    //}
+    //};
+    //}
 
     Ok(())
 }
