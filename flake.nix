cd5a78bbf3998495b4fe42fa223838d8b23ceb30
Yusuf Bera Ertan <y.bera003.06@protonmail.com>
2021-04-18 19:29:40 +0300

    build(nix): use nix-cargo-integration

--- flake.nix
+++ flake.nix
@@ -1,47 +1,13 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-20.09";
-    utils.url = "github:numtide/flake-utils";
-  };
-
-  inputs.rust-overlay = {
-    url = "github:oxalica/rust-overlay";
-  };
-
-  inputs.naersk.url = "github:nmattia/naersk";
-
-  outputs = { self, nixpkgs, utils, rust-overlay, naersk }:
-  utils.lib.eachDefaultSystem (system:
-  let pkgs = import nixpkgs {
-    inherit system;
-    overlays = [
-      rust-overlay.overlay
-      (self: super: {
-        rustc = self.latest.rustChannels.nightly.rust;
-        cargo = self.latest.rustChannels.nightly.rust;
-      })
-    ];
-  };
-  naersk-lib = naersk.lib."${system}".override {
-    rustc = pkgs.rustc;
-    cargo = pkgs.cargo;
-    rustfmt = pkgs.rustfmt;
-    clippy = pkgs.clippy;
-  };
-  in rec {
-    packages.flake-generator = naersk-lib.buildPackage {
-      pname = "flake-generator";
-      root = ./.;
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixCargoIntegration = {
+      url = "github:yusdacra/nix-cargo-integration";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
-    defaultPackage = packages.flake-generator;
+  };
 
-    apps.flake-generator = utils.lib.mkApp {
-      drv = packages.flake-generator;
-    };
-    defaultApp = apps.flake-generator;
-
-    devShell = pkgs.mkShell {
-      nativeBuildInputs = with pkgs; [ rustc cargo rustfmt clippy];
-    };
-  });
+  outputs = inputs: inputs.nixCargoIntegration.lib.makeOutputs {
+    root = ./.;
+  };
 }
