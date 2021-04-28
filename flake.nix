ab91de0f0fdcd2005776e5d600ee9f0300f7c047
Yusuf Bera Ertan <y.bera003.06@protonmail.com>
2021-04-28 03:23:04 +0300

    build: update flake deps, change ci to fit new nix-cargo-integration checks, switch to crate2nix build platform

--- flake.nix
+++ flake.nix
@@ -1,27 +1,15 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    rustOverlay = {
-      url = "github:oxalica/rust-overlay";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
     nixCargoIntegration = {
       url = "github:yusdacra/nix-cargo-integration";
       inputs.nixpkgs.follows = "nixpkgs";
-      inputs.rustOverlay.follows = "rustOverlay";
     };
   };
 
   outputs = inputs@{ nixpkgs, nixCargoIntegration, ... }:
     nixCargoIntegration.lib.makeOutputs {
       root = ./.;
-      overrides = {
-        common = prev: {
-          env = prev.env // {
-            TEMPLATER_FMT_BIN = "${prev.pkgs.nixpkgs-fmt}/bin/nixpkgs-fmt";
-            TEMPLATER_CARGO_BIN = "${prev.pkgs.rustc}/bin/cargo";
-          };
-        };
-      };
+      buildPlatform = "crate2nix";
     };
 }
