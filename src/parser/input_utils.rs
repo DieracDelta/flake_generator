4b27f8fbcf54454a8f8e24245e3fa2fd5c59ed12
Justin Restivo <justin.p.restivo@gmail.com>
2021-05-02 16:21:30 -0400

    idiomatic attribute set generation

--- src/parser/input_utils.rs
+++ src/parser/input_utils.rs
@@ -1,9 +1,11 @@
 use crate::parser::utils::{string_to_node, NixNode};
 use anyhow::{anyhow, bail};
 use either::Either;
-use rnix::{types::*, NixLanguage, StrPart, SyntaxKind::*};
-use rowan::{api::SyntaxNode, GreenNode, GreenNodeBuilder, Language};
-use std::convert::identity;
+use rnix::{types::*, NixLanguage, StrPart, SyntaxKind::*, parse};
+use rowan::{api::SyntaxNode, GreenNode, GreenNodeBuilder, Language, NodeOrToken, GreenToken};
+use std::string::ToString;
+use nixpkgs_fmt::reformat_node;
+use std::ops::Deref;
 
 #[derive(Debug, Clone, Eq, PartialEq)]
 struct Input {
@@ -11,24 +13,86 @@ struct Input {
     is_flake: bool,
 }
 
-/// inputs are: [(lhs, rhs), ...]
-fn create_attr(attr_pairs: Vec<(String, Either<String, NixNode>)>) -> GreenNode {
-    attr_pairs
-        .iter()
-        .map(|(lhs, rhs)| (lhs, rhs.as_ref().either(identity, |x| x.to_string())));
+pub fn wrap_root(node: GreenNode) -> NixNode {
+    let kind: rowan::SyntaxKind = NixLanguage::kind_to_raw(NODE_ROOT);
+    let root = GreenNode::new(kind, (vec![NodeOrToken::Node(node)]));
+    SyntaxNode::new_root(root)
+}
+
+pub fn new_string(s : String) -> GreenNode {
     let mut node = GreenNodeBuilder::new();
-    let kind: rowan::SyntaxKind = NixLanguage::kind_to_raw(NODE_ATTR_SET);
-    node.start_node(NixLanguage::kind_to_raw(NODE_ATTR_SET));
+    let kind: rowan::SyntaxKind = NixLanguage::kind_to_raw(NODE_STRING);
+    node.start_node(kind);
+    let start_string_kind: rowan::SyntaxKind = NixLanguage::kind_to_raw(TOKEN_STRING_START);
+    node.token(start_string_kind, "\"");
+    let string_content: rowan::SyntaxKind = NixLanguage::kind_to_raw(TOKEN_STRING_CONTENT);
+    node.token(string_content, &s);
+    let end_string_kind: rowan::SyntaxKind = NixLanguage::kind_to_raw(TOKEN_STRING_END);
+    node.token(end_string_kind, "\"");
     node.finish_node();
     node.finish()
 }
 
+pub fn new_key(s : String) -> GreenNode {
+    let kind: rowan::SyntaxKind = NixLanguage::kind_to_raw(NODE_KEY);
+    let children = vec![NodeOrToken::Node(new_string(s))];
+    GreenNode::new(kind, children)
+}
+
+pub fn gen_key_value(key: String, value: String) -> GreenNode {
+    let key_node : GreenNode = new_key(key);
+    let value_node : GreenNode = new_string(value);
+    new_key_value(key_node, value_node)
+}
+
+pub fn new_key_value(key : GreenNode, value: GreenNode) -> GreenNode {
+    let kind = NixLanguage::kind_to_raw(NODE_KEY_VALUE);
+    let assign_kind = NixLanguage::kind_to_raw(TOKEN_ASSIGN);
+    let whitespace_kind = NixLanguage::kind_to_raw(TOKEN_WHITESPACE);
+    let semicolon_kind = NixLanguage::kind_to_raw(TOKEN_SEMICOLON);
+    let children = vec![
+        NodeOrToken::Node(key),
+        NodeOrToken::Token(GreenToken::new(whitespace_kind, " ")),
+        NodeOrToken::Token(GreenToken::new(assign_kind, "=")),
+        NodeOrToken::Token(GreenToken::new(whitespace_kind, " ")),
+        NodeOrToken::Node(value),
+        NodeOrToken::Token(GreenToken::new(semicolon_kind, ";")),
+    ];
+    GreenNode::new(kind, children)
+}
+
+pub fn gen_attr_set(attr_pairs: Vec<(String, String)>) -> GreenNode{
+    let new_attr_pairs : Vec<(GreenNode, GreenNode)> =
+        attr_pairs.iter().map(|(key, value)| (new_key(key.to_string()), new_string(value.to_string()))).collect();
+    new_attr_set(new_attr_pairs)
+}
+
+// TODO give all the tokens their own constructors
+/// inputs are: [(lhs, rhs), ...]
+pub fn new_attr_set(attr_pairs: Vec<(GreenNode, GreenNode)>) -> GreenNode {
+    let pairs : Vec<NodeOrToken<_,_>> = attr_pairs
+        .iter()
+        .map(move |(k, v)| {
+            NodeOrToken::Node(new_key_value(k.clone(), v.clone()))
+        }).collect::<Vec<_>>();
+    let open_curly_kind = NixLanguage::kind_to_raw(TOKEN_CURLY_B_OPEN);
+    let close_curly_kind = NixLanguage::kind_to_raw(TOKEN_CURLY_B_CLOSE);
+    let whitespace_kind = NixLanguage::kind_to_raw(TOKEN_WHITESPACE);
+    let attr_set_kind = NixLanguage::kind_to_raw(NODE_ATTR_SET);
+    let mut token_vec = Vec::new();
+    token_vec.push(vec![NodeOrToken::Token(GreenToken::new(open_curly_kind, "{"))]);
+    token_vec.push(pairs);
+    token_vec.push(vec![NodeOrToken::Token(GreenToken::new(close_curly_kind, "}"))]);
+    let tokens = token_vec.iter().flatten().cloned().collect::<Vec<NodeOrToken<_,_>>>();
+    GreenNode::new(attr_set_kind, tokens)
+}
+
 impl From<Input> for GreenNode {
     fn from(item: Input) -> Self {
-        let mut node = GreenNodeBuilder::new();
-        let kind: rowan::SyntaxKind = NixLanguage::kind_to_raw(NODE_ATTR_SET);
-        node.start_node(kind);
-        node.finish_node();
-        node.finish()
+        let inputs = vec![
+            ("url".to_string(), item.url),
+            ("flake".to_string(), item.is_flake.to_string())
+        ];
+        gen_attr_set(inputs)
     }
 }
--- src/parser/input_utils_tests.rs
+++ src/parser/input_utils_tests.rs
@@ -1,4 +1,32 @@
-#[cfg(test)]
-mod tests {
-    use super::*;
+use crate::parser::input_utils::{new_string, wrap_root, new_key, gen_attr_set};
+use crate::parser::utils::{node_to_string, string_to_node};
+
+use rnix::{types::*, SyntaxKind::*};
+
+#[test]
+pub fn check_new_string() {
+    let phrase = "hello_world".to_string();
+    let n = new_string(phrase.clone());
+    let root = wrap_root(n);
+    let result = Root::cast(root).unwrap();
+    //let ast = rnix::parse(&phrase)
+        //.as_result()
+        //.map(|ast| ast.root()).unwrap();
+    //assert_eq!(format!("{}", result.dump()).trim(), format!("{}", ast.dump()).trim())
+    assert_eq!(format!("{}", result.dump()).trim(), "NODE_ROOT 0..13 {\n  NODE_STRING 0..13 {\n    TOKEN_STRING_START(\"\\\"\") 0..1\n    TOKEN_STRING_CONTENT(\"hello_world\") 1..12\n    TOKEN_STRING_END(\"\\\"\") 12..13\n  }\n}")
 }
+
+
+#[test]
+pub fn check_new_attr_set() {
+    let attrset = vec![("test1".to_string(), "value1".to_string()), ("test2".to_string(), "value2".to_string())];
+    let result = gen_attr_set(attrset);
+    let root = wrap_root(result);
+    // TODO separate this out into a dump ast method..
+    //let result = Root::cast(root).unwrap();
+    //let r_string = format!("{}", result.dump());
+    //let r_string = r_string;
+    //println!("ast: {}", r_string.clone());
+    assert_eq!(root.to_string(), "{\"test1\" = \"value1\";\"test2\" = \"value2\";}");
+}
+
